96cd2dac6bc118cdeb5c3c2e0c86a97f7dc8db63
Sean Meyers <smmeyers@ualberta.ca>
2024-01-27 23:12:59 -0700

    Followed tutorial and made very basic scuff battle scene (https://www.youtube.com/watch?v=ifXGvlAn0bY). Prolly need to make some stuff into seperate scenes and clean up the code a bit to be more reuseable. Next step is to make an interface for the the rolling and using of multiple dice, and to make the scene work with multiple enemies, and to put a temp player sprite and and and...

---
+++ src/battle.gd
@@ -0,0 +1,87 @@
+extends Control
+
+@export var enemy: Resource = null
+
+signal textbox_closed
+
+var player_health = 0
+var enemy_health = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	set_health($EnemyContainer/ProgressBar, enemy.health, enemy.health)
+	set_health($PlayerPanel/PlayerData/ProgressBar, State.player_health, State.player_health_max)
+	$EnemyContainer/Enemy.texture = enemy.texture
+	
+	player_health = State.player_health
+	enemy_health = enemy.health
+	
+	$Textbox.hide()
+	$ActionsPanel.hide()
+	
+	display_text("A wild %s appears!" % enemy.name.to_upper())
+	await textbox_closed
+	$ActionsPanel.show()
+	
+func _input(event):
+	if (Input.is_action_just_pressed("ui_accept") or Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT)) and $Textbox.visible:
+		$Textbox.hide()
+		emit_signal("textbox_closed")
+	
+func display_text(text):
+	$Textbox.show()
+	$Textbox/Label.text = text
+	
+func set_health(progress_bar, hp, hp_max):
+	progress_bar.value = hp
+	progress_bar.max_value = hp_max
+	progress_bar.get_node("Label").text = "hp: %d/%d" % [hp, hp_max]
+	
+func enemy_turn():
+	display_text("Oh noes! %s is coming for you!" % enemy.name)
+	await textbox_closed
+	
+	player_health = max(0, player_health - enemy.damage)
+	set_health($PlayerPanel/PlayerData/ProgressBar, player_health, State.player_health_max)
+	
+	#$AnimationPlayer.play("enemy_damaged")   #TODO: Make a temp player hurt anim... need a godot logo for the player first...
+	#await $AnimationPlayer.animation_finished
+	
+	display_text("%s dealt %d damage!" % [enemy.name, enemy.damage])
+	await textbox_closed
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_run_pressed():
+	display_text("You coward! Execute game crash!")
+	await textbox_closed
+	await get_tree().create_timer(0.5).timeout
+	get_tree().quit()
+
+
+func _on_attack_pressed():
+	display_text("Attacking! dun dun dun!")
+	await textbox_closed
+	
+	enemy_health = max(0, enemy_health - State.player_damage)
+	set_health($EnemyContainer/ProgressBar, enemy_health, enemy.health)
+	
+	$AnimationPlayer.play("enemy_damaged")
+	await $AnimationPlayer.animation_finished
+	
+	display_text("Dealt %d damage!" % State.player_damage)
+	await textbox_closed
+	
+	if enemy_health == 0:
+		display_text("%s was defeated! This is bullshit! I'm not playing anymore! *crashes game*" % enemy.name)
+		await textbox_closed
+		# TODO: temp enemy death anim. 
+		
+		await get_tree().create_timer(0.5).timeout
+		get_tree().quit()
+	
+	enemy_turn()
