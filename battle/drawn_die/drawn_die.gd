bde794ed1931366617b29009bca52d56f2e62fa6
Boris <nedev@ualberta.ca>
2024-03-10 12:52:56 -0600

    Changed the visual of diesides in battle
    diesides now have an orange border when selected for attacking and are greyed out when they have been given an enemy to attack. Battle scene dice now also have their own styleboxes instead of using inv_die_frame's styleboxes

--- battle/drawn_die/drawn_die.gd
+++ battle/drawn_die/drawn_die.gd
@@ -1,6 +1,12 @@
 extends InvDieFrame
 class_name DrawnDie
 
+## paths to the styleboxes we need
+var focused_press_path : StyleBox = preload("res://battle/drawn_die/styles/button_battle_focused_pressed.tres")
+var pressed_path : StyleBox = preload("res://battle/drawn_die/styles/button_battle_pressed.tres")
+## name of the styleboxes we wish to change
+var pressed_style = "pressed"
+var normal_style = "normal"
 
 var data: DrawnDieData:
 		set (ddd):
@@ -12,9 +18,9 @@ var side : DieSide:
 			data.side = side
 			die_type.text = "%d" % side.value
 			# Set the color based on the effect of the side.
-			var new_stylebox_normal = get_theme_stylebox("normal").duplicate()
+			var new_stylebox_normal = get_theme_stylebox(normal_style).duplicate()
 			new_stylebox_normal.bg_color = side.element.color
-			add_theme_stylebox_override("normal", new_stylebox_normal)
+			add_theme_stylebox_override(normal_style, new_stylebox_normal)
 		get:
 			return data.side
 var roll: int:
@@ -56,6 +62,7 @@ static func instantiate(node_path: String, parent: Node, _die: Die, battle_conte
 ## called when the player selects a die in their hand. allows player to target actors.
 func _on_toggled(toggled_on):
 	if toggled_on:
+		make_focused_pressed()
 		var targets = data.battle.enemies.duplicate()
 		targets.append(data.battle.player)
 
@@ -73,7 +80,27 @@ func _on_toggled(toggled_on):
 		target = await data.battle.target_selected
 		
 		selected_action = data.ATTACK if target is BattleEnemy else data.DEFEND
-
+		
+		make_pressed()
 		# Clean up once targeting is finished
 		for option in targets:
 			option.toggle_target_mode(false, data.battle.target_selected)
+			
+
+## A function that changes the style box of the drawn_die
+## in order to make it clear what die is currently selected
+## effectively mimics what focus does visually
+func make_focused_pressed():
+	button.remove_theme_stylebox_override(pressed_style)
+	button.add_theme_stylebox_override(pressed_style, focused_press_path)
+	var new_stylebox_pressed = get_theme_stylebox(pressed_style)
+	new_stylebox_pressed.bg_color = side.element.color
+
+## A function that changes the style box of the drawn_die
+## once it is no longer the selected die
+func make_pressed():
+	button.remove_theme_stylebox_override(pressed_style)
+	button.add_theme_stylebox_override(pressed_style, pressed_path)
+
+
+	
--- modules/inventory/diceinv/inv_die_frame.gd
+++ modules/inventory/diceinv/inv_die_frame.gd
@@ -19,7 +19,11 @@ func update(die: Die):
 	die_ref = die
 	
 func _on_button_pressed():
-	print("I am a " + die_type.text + " with the following sides: ")
-	for dieside in die_ref.sides:
-		print(dieside.element)
-	die_clicked.emit(die_ref)
+	button.grab_focus()
+	if(die_ref != null):
+		print("I am a " + die_type.text + " with the following sides: ")
+		for dieside in die_ref.sides:
+			print(dieside.element)
+		die_clicked.emit(die_ref)
+	else:
+		print("Im just a frame with no information big dog")
