51893b22fa290835c6b28b49ecbedc8341d29ddf
Sean Meyers <smmeyers@ualberta.ca>
2024-03-19 18:41:41 -0600

    icons uwu

--- battle/temp_status_effect_icon.gd
+++ battle/temp_status_effect_icon.gd
@@ -1,18 +1,29 @@
 class_name TempSEIcon extends Button
 
+# prolly temp: TODO: make classes/seperate scenes for each icon or something idk.
+@export var par: AtlasTexture
+@export var ign: AtlasTexture
+@export var def: AtlasTexture
+@export var poi: AtlasTexture
+
+
+
 var strength: int = 1:
 	set (value):
 		strength = value
-		text = _fstring % [TempSEIcon._to_rom_num(strength), remaining_turns]
+		#text = _fstring % [TempSEIcon._to_rom_num(strength), remaining_turns]
+		text = _fstring % [remaining_turns]
 var remaining_turns: int = 0:
 	set (value):
 		remaining_turns = value
-		text = _fstring % [TempSEIcon._to_rom_num(strength), remaining_turns]
+		#text = _fstring % [TempSEIcon._to_rom_num(strength), remaining_turns]
+		text = _fstring % [remaining_turns]
 var beneficial: bool
 var color: Color
 var effect: StatusEffect
 
-static var _fstring := "%s: %d"		# Strength (roman num): turns left
+#static var _fstring := "%s: %d"		# Strength (roman num): turns left
+static var _fstring := "%d"
 
 ## Loads, attaches to parent, and initializes required parameters all in one go.
 static func instantiate(node_path: String, parent: Node, _effect: StatusEffect):
@@ -54,13 +65,25 @@ func _ready():
 	text = _fstring % [TempSEIcon._to_rom_num(strength), remaining_turns]
 	match effect._type:
 		StatusEffect.EffectType.PARALYSIS:
-			self_modulate = Color.YELLOW
+			#self_modulate = Color.YELLOW
+			#icon.region = Rect2(0, 0, 16, 16)	# 8c3cee
+			icon = par
+			add_theme_color_override("font_color", Color("#8c3cee"))
 		StatusEffect.EffectType.AUTODEFENSE:
-			self_modulate = Color.GRAY
+			#self_modulate = Color.GRAY
+			#icon.region = Rect2(0, 96, 16, 16) # 9db3bf
+			icon = def
+			add_theme_color_override("font_color", Color("#9db3bf"))
 		StatusEffect.EffectType.IGNITED:
-			self_modulate = Color.DARK_RED
+			#self_modulate = Color.DARK_RED
+			#icon.region = Rect2(0, 32, 16, 16) # a90909
+			icon = ign
+			add_theme_color_override("font_color", Color("#a90909"))
 		StatusEffect.EffectType.POISONED:
-			self_modulate = Color.WEB_PURPLE
+			#self_modulate = Color.WEB_PURPLE
+			#icon.region = Rect2(0, 64, 16, 16) # 239063
+			icon = poi
+			add_theme_color_override("font_color", Color("#239063"))
 
 
 func update():
