5495382f20d917281e446f4bbe4bbdf218f5a9b5
Aidan <batten@ualberta.ca>
2024-04-09 16:54:32 -0600

    Revert to 07de32e3

--- modules/elements/basic_element.gd
+++ modules/elements/basic_element.gd
@@ -5,13 +5,9 @@ class_name Basic
 func _init():
 	element = ElementType.NONE
 	value = 2
-	stack_roll = false
 	effect = StatusEffect.EffectType.BASEEFFECT
 	color = Color.BEIGE
 	
 ## String representation of the element
 func _to_string():
 	return "Basic"
-
-func info():
-	return "Attack/Defend with the number rolled."
--- modules/elements/steel_element.gd
+++ modules/elements/steel_element.gd
@@ -5,14 +5,9 @@ class_name Steel
 func _init():
 	element = ElementType.STEEL
 	value = 2
-	stack_roll = true
 	effect = StatusEffect.EffectType.AUTODEFENSE
 	color = Color.LIGHT_STEEL_BLUE
 	
 ## String representation of the element
 func _to_string():
 	return "Steel"
-
-func info():
-	var steel  = "[color=Gray]Steel[/color]"
-	return "%s gives the player the current %s stack as defense on their turn." % [steel, steel]
