5e3205d2fef4f94ae1e7308608cf9dafe6ff0269
Boris <nedev@ualberta.ca>
2024-04-09 18:41:17 -0600

    Made a super easy encounter for easy testing

--- battle/battle_item_drop.gd
+++ battle/battle_item_drop.gd
@@ -4,11 +4,13 @@ extends Control
 @export var common_loot_table: LootTable
 @export var uncommon_loot_table: LootTable
 
-@onready var textbox_controller := $"VBoxContainer/HBoxContainer2/Textbox Controller"
+@onready var textbox_controller := $"VBoxContainer/Textbox Controller"
 @onready var inventory := %item_drop_display
+@onready var loot_drop_container := %LootDropContainer
 @onready var inv_dice_visual = preload("res://modules/inventory/diceinv/inv_die_frame.tscn")
 @onready var inv_side_visual = preload("res://modules/inventory/diceinv/inv_dieside_frame.tscn")
 @onready var side_name = "Sides"
+@onready var skip_rewards = %skip_reward
 
 var selected_die
 var dropped_die_array: Array[Die] = []
@@ -21,33 +23,13 @@ func _on_dialogue_transitiond(from_beat: DialogueBeat, destination_beat: String,
 	match from_beat.unique_name:
 		"confirm":
 			if from_choice == 0:
-				print("player said yes")
-				await textbox_controller.quick_beat("confirm2", [], _on_dialogue_transitiond)
-				await textbox_controller.quick_beat("confirm3", [], _on_dialogue_transitiond)
+				PlayerData.dice_bag.append(selected_die)
+				await textbox_controller.quick_beat("drop_confirm", [], _on_dialogue_transitiond)
+				exit_drop_screen()
 			else:
 				print("player said no")
-				#choices_container.visible = false
-				
-				#Close preview
-				#inventory.open()
-				#inventory.current_tab = "Dropped Items"
 				inventory.current_tab = 0
 				await textbox_controller.quick_beat("directions", [], _on_dialogue_transitiond)
-	
-	match from_beat.unique_name:
-		"confirm3":
-			if from_choice == 0:
-				PlayerData.dice_bag.append(selected_die)
-				#inventory.is_side_view_open = false
-				#inventory.close()
-				await textbox_controller.quick_beat("drop_confirm", [], _on_dialogue_transitiond)
-				# Change to map once done
-			else:
-				await textbox_controller.quick_beat("no_die", [], _on_dialogue_transitiond)
-			
-			queue_free()
-			get_node("/root/Map").show()
-
 
 func show_sides(die : Die):
 	var side_view
@@ -65,12 +47,10 @@ func show_sides(die : Die):
 
 func select_die(frame):
 	selected_die = frame
-	
 	# Open side preview
 	show_sides(frame)
-	
 	# Display prompt
-	#choices_container.visible = true
+	textbox_controller.visible=true
 	await textbox_controller.quick_beat("confirm", [], _on_dialogue_transitiond)
 
 
@@ -79,17 +59,21 @@ func _ready():
 	get_node("/root/Map").hide()
 	# Generate 3 random battle drop items from a randomly selected loot table
 	item_generation()
-	
 	## setup for dice inventory tab
 	inventory.make_tab("Dropped Items", dropped_die_array, inv_dice_visual)
 	## setup for die sides inventory tab
 	inventory.make_tab(side_name, [], inv_side_visual)
-	
+	skip_rewards.pressed.connect(skip_reward)
 	inventory.return_clicked.connect(select_die)
 	await textbox_controller.quick_beat("congrats", [], _on_dialogue_transitiond)
-	inventory.open()
 	await textbox_controller.quick_beat("directions", [], _on_dialogue_transitiond)
+	inventory.open()
+	skip_rewards.visible = true
+	
 
+func skip_reward():
+	await textbox_controller.quick_beat("no_die", [], _on_dialogue_transitiond)
+	exit_drop_screen()
 
 func item_generation():
 	# Use probability to determine only ONE loot table 
@@ -115,3 +99,7 @@ func item_generation():
 			dropped_die_array.append(drop)
 		
 		print("index of dropped die array:", index_of_dropped_die)
+		
+func exit_drop_screen():
+	queue_free()
+	get_node("/root/Map").show()
