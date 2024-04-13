758e0c6acb04899ab9ae7e8457eab35f4e43774f
Boris <nedev@ualberta.ca>
2024-04-13 17:35:08 -0600

    Moved inventory overlay setup out of map.gd

--- map/map.gd
+++ map/map.gd
@@ -11,38 +11,12 @@ var points = []
 #var bg_texture 	= preload("res://assets/textures/map noise paper ninepatch.png")
 var map_scene 	= preload("res://map/MapTree.tscn")
 
+@onready var canvas_layer = %CanvasLayer
+
 #@export var scroll_cont : ScrollContainer
 @export var bg 			: NinePatchRect
 
-## Inventory
-@onready var inv_dice_visual = preload("res://modules/inventory/diceinv/inv_die_frame.tscn")
-@onready var inv_side_visual = preload("res://modules/inventory/diceinv/inv_dieside_frame.tscn")
-@onready var side_name = "Sides"
-
-@onready var canvas_layer = $CanvasLayer
-@onready var player_status = %"Player Status"
-@onready var inventory = %"Display Box"
-@export var temp_dice_bag_init: PlayerDataInit
-var inventory_open = false
-
-func _ready():
-	
-	if PlayerData.dice_bag.size() == 0:
-		PlayerData.dice_bag = temp_dice_bag_init.dice.duplicate(true)
-	
-	player_status.dice_remaining = PlayerData.dice_bag.size()
-	
-	## setup for dice inventory tab
-	inventory.make_tab("In Bag", PlayerData.dice_bag, inv_dice_visual)
-	## setup for die sides inventory tab
-	inventory.make_tab(side_name, [], inv_side_visual)
-	
-	## connect dice bag button to inventory
-	player_status.bag_button.pressed.connect(track_inventory)
-	## connect frame clicks to display sides
-	inventory.return_clicked.connect(show_sides)
-	
-	
+func _ready():	
 	setup()
 	
 func setup():
@@ -115,36 +89,7 @@ func add_nodes():
 			#node.text = text
 			
 			bg.add_child(node)
-
-
-## simple function that changes what is displayed on the sides tab
-func show_sides(die : Die):
-	var side_view
-	for child in inventory.get_children():
-		if child.tabobj_ref.get_tab_title() == side_name:	 #hardcoded cause bro this shit is ass
-			side_view = child
-	if side_view == null:
-		return
-	else:
-		side_view.new_frames(die.sides)
-		inventory.current_tab = side_view.get_index()
-
-func _unhandled_key_input(event: InputEvent) -> void:
-	if event.is_action_pressed("I") and inventory_open == false:
-		inventory.open()
-		inventory_open = true
-	elif event.is_action_pressed("I") and inventory_open == true:
-		inventory.hide()
-		inventory_open = false
-
-func track_inventory():
-	if inventory.visible == false:
-		inventory.open()
-		inventory_open = true
-	elif inventory.visible == true:
-		inventory.close()
-		inventory_open = false
-
+		
 func _on_visibility_changed():
 	if visible:
 		for map_node in bg.get_children():
